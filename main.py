2f49124c46511630eb136c5ee0255616c9afc8a0
Kirill Ibragimov <hexchap@gmail.com>
2025-05-08 01:16:35 +0300

    Prepare for docker

--- main.py
+++ main.py
@@ -76,5 +76,5 @@ configure_tortoise(application)
 include_routers(application)
 
 
-if __name__ == "__main__":
-    uvicorn.run("main:application", host="0.0.0.0", port=8000, reload=True)
+# if __name__ == "__main__":
+#     uvicorn.run("main:application", host="0.0.0.0", port=8000, reload=True)
